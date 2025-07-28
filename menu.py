9d24828f1d87fa4a813971094bc7f9f32249326b
pedenrique9029 <pedrommfrv@gmail.com>
2025-07-28 16:07:35 -0300

    atualização do menu

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 import pygame
-
+from settings import LARGURA, ALTURA
 
 class Menu:
     def __init__(self, largura, altura):
@@ -8,6 +8,8 @@ class Menu:
         self.altura = altura
         self.fonte = pygame.font.SysFont(None, 48)
         self.fonte_pequena = pygame.font.SysFont(None, 36)
+        self.rect_start = pygame.rect.Rect(LARGURA/2-100, ALTURA/2+5, 200, 50)
+        self.rect_quit = pygame.rect.Rect(LARGURA/2-50, ALTURA/2+65, 100, 30)
 
     def desenhar(self, tela):
         tela.fill((0, 0, 0))  # Fundo preto
@@ -15,11 +17,8 @@ class Menu:
         titulo = self.fonte.render("WORMHOLE", True, (255, 255, 255))
         tela.blit(titulo, (self.largura // 2 - titulo.get_width() // 2, 100))
 
-        rect_start = pygame.rect.Rect(300, 250, 200, 50)
-        rect_quit = pygame.rect.Rect(350, 320, 100, 30)
-
-        pygame.draw.rect(tela, (0, 150, 100), rect_start)
-        pygame.draw.rect(tela, (150, 0, 0), rect_quit)
+        pygame.draw.rect(tela, (0, 150, 100), self.rect_start)
+        pygame.draw.rect(tela, (150, 0, 0), self.rect_quit)
         # Opções
         opcoes = [
             "Start/Resume",
@@ -28,24 +27,18 @@ class Menu:
 
         for i, opcao in enumerate(opcoes):
             texto = self.fonte_pequena.render(opcao, True, (255, 255, 255))
-            tela.blit(texto, (self.largura // 2 - texto.get_width() // 2, 265 + i * 60))
+            tela.blit(texto, (self.largura // 2 - texto.get_width() // 2, 375 + i * 50))
 
-    @staticmethod
-    def processar_eventos():
+    def processar_eventos(self):
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 return "sair"
             elif evento.type == pygame.MOUSEBUTTONDOWN:
                 click_x, click_y = evento.pos
-                if (300 < click_x < 500) and (185 < click_y < 235):
+                if (self.rect_start.left < click_x < self.rect_start.right) and (self.rect_start.top < click_y < self.rect_start.bottom):
                     return "fase1"
-                elif (350 < click_x < 450) and (247 < click_y < 277):
+                elif (self.rect_quit.left < click_x < self.rect_quit.right) and (self.rect_quit.top < click_y < self.rect_quit.bottom):
                     return "sair"
-            elif evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_ESCAPE:
-                    return "sair"
-                elif evento.key == pygame.K_1:
-                    return "fase1"
         return None
 
     def executar(self, tela):
