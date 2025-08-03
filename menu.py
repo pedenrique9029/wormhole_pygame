97e77f571f05c4090245073ebb24527fa951f175
pedenrique9029 <pedrommfrv@gmail.com>
2025-08-03 13:29:43 -0300

    alteração nas fontes do menu

--- menu.py
+++ menu.py
@@ -6,6 +6,7 @@ class Menu:
         self.rodando = True
         self.largura = largura
         self.altura = altura
+        self.fonte_grande = pygame.font.SysFont(None, 52)
         self.fonte = pygame.font.SysFont(None, 48)
         self.fonte_pequena = pygame.font.SysFont(None, 36)
         self.rect_start = pygame.rect.Rect(LARGURA/2-100, ALTURA/2-20, 200, 50)
@@ -15,20 +16,17 @@ class Menu:
         imagem_fundo  = pygame.image.load('assets/buraco_negro_pixelizado_espelhado.jpg')# Imagem de fundo
         imagem_fundo = pygame.transform.rotate(imagem_fundo, 90)
         tela.blit(imagem_fundo, (-120, -60))
-        titulo = self.fonte.render("WORMHOLE", True, (255, 255, 255))
+        titulo = self.fonte_grande.render("WORMHOLE", True, (255, 255, 255))
         tela.blit(titulo, (self.largura // 2 - titulo.get_width() // 2, 220))
 
         pygame.draw.rect(tela, (0, 0, 0), self.rect_start)
         pygame.draw.rect(tela, (0, 0, 0), self.rect_quit)
-        # Opções
-        opcoes = [
-            "Start",
-            "Quit"
-        ]
 
-        for i, opcao in enumerate(opcoes):
-            texto = self.fonte_pequena.render(opcao, True, (255, 255, 255))
-            tela.blit(texto, (self.largura // 2 - texto.get_width() // 2, 350 + i * 80))
+        text_start = self.fonte.render("Iniciar", True, (255, 255, 255))
+        tela.blit(text_start, (self.largura // 2 - text_start.get_width() // 2, 350))
+
+        text_quit = self.fonte_pequena.render("Sair", True, (255, 255, 255))
+        tela.blit(text_quit, (self.largura // 2 - text_quit.get_width() // 2, 420))
 
     def processar_eventos(self):
         for evento in pygame.event.get():
